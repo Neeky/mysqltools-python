345640ff830f2eef1c0d70308bf569aec6b3013b
Neeky <1721900707@qq.com>
2019-07-27 13:38:13 +0800

    2.19.07.27

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from distutils.core import setup
 
+olds_scripts = ['bin/mtlsmonitor','bin/mtlsbackup','bin/mtlslog','bin/mtlsanalysis',
+               'bin/mtlshttp','bin/mtlsbigfiles','bin/mtlsdeleterows']
+
+news_scripts = ['bin/mtls-big-files','bin/mtls-delete-rows','bin/mtls-file-truncate',
+                'bin/mtls-http','bin/mtls-log','bin/mtls-monitor','bin/mtls-backup']
+
+scripts = olds_scripts + news_scripts
+
 setup(name='mysqltools-python',
-      version='2.19.07.26',
-      scripts=['bin/mtlsmonitor','bin/mtlsbackup','bin/mtlslog','bin/mtlsanalysis',
-               'bin/mtlshttp','bin/mtlsbigfiles','bin/mtlsdeleterows'],
+      version='2.19.07.28',
+      scripts=scripts,
       packages=['mtls','mtls.kits'],
       maintainer='Neeky',
       maintainer_email='neeky@live.com',
