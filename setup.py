a58033f85357f572182f3c3dc8565ba15e268612
Neeky <1721900707@qq.com>
2019-08-07 15:35:48 +0800

    2.19.08.01

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ olds_scripts = ['bin/mtlsmonitor','bin/mtlsbackup','bin/mtlslog','bin/mtlsanalys
 
 news_scripts = ['bin/mtls-big-files','bin/mtls-delete-rows','bin/mtls-file-truncate',
                 'bin/mtls-http','bin/mtls-log','bin/mtls-monitor','bin/mtls-backup',
-                'bin/mtls-perf-bench']
+                'bin/mtls-perf-bench','bin/mtls-kill-all-conections']
 
 scripts = olds_scripts + news_scripts
 
 setup(name='mysqltools-python',
-      version='2.19.07.30',
+      version='2.19.08.01',
       scripts=scripts,
       packages=['mtls','mtls.kits'],
       maintainer='Neeky',
